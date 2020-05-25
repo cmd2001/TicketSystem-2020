8710c2b9b1f8bf5763196611c5dcfab3a4f33992
battlin <battlin6@gmail.com>
2020-05-25 14:17:00 +0800

    init again

---
+++ BPlusTree/database.hpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <cmath>
+#include <string>
+#include "include/List.h"
+
--- main.cpp
+++ main.cpp
@@ -2,7 +2,13 @@
 #include <cstdio>
 #include <cstring>
 #include <cmath>
+#include <string>
+#include "include/List.h"
 
 int main(){
     std::cout<<"cmd2001nb!"<<std::endl;
+    List<int> X;
+    X.push_back(1);
+    X.push_back(2);
+    cout<<X;
 }
