abf33193fb75a5a0f6480e71e8fd7623c8ba2172
Amagi_Yukisaki <xingzeyu@sjtu.edu.cn>
2020-06-09 21:23:19 +0800

    Frontend Dev

--- app.py
+++ app.py
@@ -1,25 +1,178 @@
-from flask import Flask, render_template, request, redirect, make_response, send_from_directory
-from ticket import Ticket
-from ticket import Constant
+from flask import Flask, render_template, request, redirect, make_response
+from ticket import Ticket, Constant, cookiePool
 
 app = Flask(__name__)
 ticket = Ticket(None)
 
-@app.route('/add_user', methods=("GET", "POST"))
-def add_user():
-    if request.method == "GET":
-        return render_template('form.html', form = Constant.add_user_form)
-    form = request.form.to_dict()
-    ret = ticket.add_user(form['cur_username'], form['username'], form['password'], form['name'], form['mailAddr'])
-    if ret:
-        return render_template('success.html', form = Constant.add_user_form)
-    else:
-        return render_template('failure.html', form = Constant.add_user_form)
+userPool = cookiePool()
 
+def checkCookie():
+    userID = request.cookies.get('id')
+    return userPool.check(userID)
+
+@app.route('/login', methods=("GET", "POST"))
+def login():
+    if checkCookie():
+        return redirect('/')
+
+    if request.method == "GET":
+        return render_template('form.html', form = Constant.login_form, form_path = '/login', title = 'User Login')
+
+    form = request.form.to_dict()
+    check = ticket.login(form['username'], form['password'])
+    if not check:
+        return render_template('failure.html', form = Constant.login_form, form_path = '/login', title = 'User Login')
+    else:
+        ret = make_response(redirect('/'))
+        ret.set_cookie('id', userPool.push(form['username']))
+        return ret
+
+@app.route('/logout')
+def logout():
+    userID = request.cookies.get('id')
+    if not checkCookie():
+        return redirect('/')
+
+    name = userPool.query(userID)
+    ticket.logout(name)
+    userPool.erase(userID)
+    return redirect('/')
+
+
+def getUsername():
+    userID = request.cookies.get('id')
+    return userPool.query(userID)
+
+# todo: check whether system has initialized
 
 @app.route('/')
 def root():
-    return 'Hello World!'
+    userID = request.cookies.get('id')
+    if not checkCookie():
+        return render_template('root_login.html')
+    return render_template('root.html', username = userPool.query(userID))
+
+
+
+@app.route('/add_user', methods=("GET", "POST"))
+def add_user():
+    if request.method == "GET":
+        return render_template('form.html', form = Constant.add_user_form, form_path = '/add_user', title = 'Add User')
+    form = request.form.to_dict()
+    ret = ticket.add_user(getUsername(), form['username'], form['password'], form['name'], form['mailAddr'], form['privilege'])
+    if ret:
+        return render_template('success.html', form = Constant.add_user_form, form_path = '/add_user', title = 'Add User')
+    else:
+        return render_template('failure.html', form = Constant.add_user_form, form_path = '/add_user', title = 'Add User')
+
+@app.route('/query_profile', methods=("GET", "POST"))
+def query_profile():
+    if request.method == "GET":
+        return render_template('form.html', form = Constant.query_profile_form, form_path = '/query_profile', title = 'Query Profile')
+    form = request.form.to_dict()
+    ret = ticket.query_profile(getUsername(), form['username'])
+    return 'Query Profile' # todo
+
+@app.route('/modify_profile', methods=("GET", "POST"))
+def modify_profile():
+    if request.method == "GET":
+        return render_template('form.html', form = Constant.modify_profile_form, form_path = '/modify_profile', title = 'Modify Profile')
+    form = request.form.to_dict()
+    ret = ticket.modify_profile(getUsername(), form['username'], form['password'], form['name'], form['mailAddr'], form['privilege'])
+    if ret:
+        return render_template('success.html', form = Constant.modify_profile_form, form_path = '/modify_profile', title = 'Modify Profile')
+    else:
+        return render_template('failure.html', form = Constant.modify_profile_form, form_path = '/modify_profile', title = 'Modify Profile')
+
+@app.route('/add_train', methods=("GET", "POST"))
+def add_train():
+    if request.method == "GET":
+        return render_template('form.html', form = Constant.add_train_form, form_path = '/add_train', title = 'Add Train')
+    form = request.form.to_dict()
+    ret = ticket.add_train(form['trainID'], len(form['stations'].split('|')), form['seatNum'], form['stations'],
+                           form['prices'], form['startTime'], form['travelTimes'], form['stopoverTimes'], form['saleDate'], form['type'])
+    if ret:
+        return render_template('success.html', form = Constant.add_train_form, form_path = '/add_train', title = 'Add Train')
+    else:
+        return render_template('failure.html', form = Constant.add_train_form, form_path = '/add_train', title = 'Add Train')
+
+@app.route('/release_train', methods=("GET", "POST"))
+def release_train():
+    if request.method == "GET":
+        return render_template('form.html', form = Constant.release_train_form, form_path = '/release_train', title = 'Release Train')
+    form = request.form.to_dict()
+    ret = ticket.release_train(form['trainID'])
+    if ret:
+        return render_template('success.html', form = Constant.release_train_form, form_path = '/release_train', title = 'Release Train')
+    else:
+        return render_template('failure.html', form = Constant.release_train_form, form_path = '/release_train', title = 'Release Train')
+
+@app.route('/query_train', methods=("GET", "POST"))
+def query_train():
+    if request.method == "GET":
+        return render_template('form.html', form = Constant.query_train_form, form_path = '/query_train', title = 'Query Train')
+    form = request.form.to_dict()
+    ret = ticket.query_train(form['trainID'], form['date'])
+    return 'Query Train' # todo
+
+@app.route('/delete_train', methods=("GET", "POST"))
+def delete_train():
+    if request.method == "GET":
+        return render_template('form.html', form = Constant.delete_train_form, form_path = '/delete_train', title = 'Delete Train')
+    form = request.form.to_dict()
+    ret = ticket.delete_train(form['trainID'])
+    if ret:
+        return render_template('success.html', form = Constant.delete_train_form, form_path = '/delete_train', title = 'Delete Train')
+    else:
+        return render_template('failure.html', form = Constant.delete_train_form, form_path = '/delete_train', title = 'Delete Train')
+
+@app.route('/query_ticket', methods=("GET", "POST"))
+def query_ticket():
+    if request.method == "GET":
+        return render_template('form.html', form = Constant.query_ticket_form, form_path = '/query_train', title = 'Query Ticket')
+    form = request.form.to_dict()
+    ret = ticket.query_ticket(form['time'], form['start'], form['end'], form['sort_param'])
+    return 'Query Ticket' # todo
+
+@app.route('/query_transfer', methods=("GET", "POST"))
+def query_transfer():
+    if request.method == "GET":
+        return render_template('form.html', form = Constant.query_transfer_form, form_path = '/query_transfer', title = 'Query Transfer')
+    form = request.form.to_dict()
+    ret = ticket.query_transfer(form['time'], form['start'], form['end'], form['sort_param'])
+    return 'Query Transfer' # todo
+
+@app.route('/buy_ticket', methods=("GET", "POST"))
+def buy_ticket():
+    if request.method == "GET":
+        return render_template('form.html', form = Constant.buy_ticket_form, form_path = '/buy_ticket', title = 'Buy Ticket')
+    form = request.form.to_dict()
+    ret = ticket.buy_ticket(getUsername(), form['trainID'], form['day'], form['ffrom'], form['to'], form['number'], form['que'])
+    return 'Buy Ticket' # todo
+
+@app.route('/query_order')
+def query_order():
+    ret = ticket.query_order(getUsername())
+    return 'Query Order' # todo
+
+@app.route('/refund_ticket', methods=("GET", "POST"))
+def refund_ticket():
+    if request.method == "GET":
+        return render_template('form.html', form = Constant.refund_ticket_form, form_path = '/refund_ticket', title = 'Refund Ticket')
+    form = request.form.to_dict()
+    ret = ticket.refund_ticket(getUsername(), form['trainID'])
+    if ret:
+        return render_template('success.html', form = Constant.refund_ticket_form, form_path = '/refund_ticket', title = 'Refund Ticket')
+    else:
+        return render_template('failure.html', form = Constant.refund_ticket_form, form_path = '/refund_ticket', title = 'Refund Ticket')
+
+@app.route('/clean')
+def clean():
+    #todo Warning
+    ticket.clean()
+    userPool.clean()
+    return redirect('/')
+
 
 if __name__ == '__main__':
     app.run(debug = True)
--- ticket.py
+++ ticket.py
@@ -2,16 +2,16 @@
 class Ticket:
     def __init__(self, para):
         self.para = para
-    def add_user(self, cur_username, username, password, name, mailAddr):
+    def add_user(self, cur_username, username, password, name, mailAddr, privilege):
         """
         :return: bool # 1 when success
         """
-        return 1
+        return
     def login(self, username, password):
         """
         :return: bool
         """
-        return
+        return 1
     def logout(self, username):
         """
         :return: bool
@@ -22,13 +22,13 @@ class Ticket:
         :return: (username，name, mailAddr, privilege)
         """
         return
-    def modify_profile(self, cur_username, username, password, name, mailAddr):
+    def modify_profile(self, cur_username, username, password, name, mailAddr, privilege):
         """
         :param cur_username: str
         :param username: str
-        :param password: (bool, str) # 1 means to modify, otherwise ignore [1].
-        :param name: (bool, str)
-        :param mailAddr: (bool, str)
+        :param password: str # not empty means to modify, otherwise ignore.
+        :param name: str
+        :param mailAddr: str
         :return: bool
         """
         return
@@ -42,7 +42,7 @@ class Ticket:
         :return: None
         """
         return
-    def query_train(self, trainID):
+    def query_train(self, trainID, date):
         """
         :return: ((trainID, type), (stations, ARRIVING_TIME, LEAVING_TIME, PRICE, SEAT)*)
         # 07-02 05:19 -> 07-02 05:24 114 1000
@@ -53,13 +53,13 @@ class Ticket:
         :return: bool
         """
         return
-    def query_ticket(self, time, station, sort_param):
+    def query_ticket(self, time, start, end, sort_param):
         """
         :return: ((trainID, FROM, LEAVING_TIME, TO, ARRIVING_TIME, PRICE, SEAT)*)
         HAPPY_TRAIN 中院 08-17 05:24 -> 下院 08-17 15:24 514 1000
         """
         return
-    def query_transfer(self, time, station, sort_param):
+    def query_transfer(self, time, start, end, sort_param):
         """
         :return: ((trainID, FROM, LEAVING_TIME, TO, ARRIVING_TIME, PRICE, SEAT)*2)
         """
@@ -88,12 +88,41 @@ class Ticket:
         return
 
 class Constant:
-    add_user_form = '''    <form role = "form" action = "/add_user" method = "post">
-        Current Username<input type = "text" name = "cur_username" required = "required"> <br>
-        Username<input type = "text" name = "username" required = "required"> <br>
-        Password<input type = "text" name = "password" required = "required"> <br>
-        Name<input  type = "text" name = "name" required = "required"> <br>
-        Mail Address<input  type = "text" name = "mailAddr" required = "required"> <br>
-        <button type = "submit">submit</button>
-    </form>'''
+    add_user_form = [('Username', 'username', 1), ('Password', 'password', 1), ('Name', 'name', 1), ('Mail Address', 'mailAddr', 1), ('Privilege', 'privilege', 1)]
+    query_profile_form = [('Username', 'username', 1)]
+    modify_profile_form = [('Username', 'username', 0), ('Password', 'password', 0), ('Name', 'name', 0), ('Mail Address', 'mailAddr', 0), ('Privilege', 'privilege', 0)]
+    add_train_form = [('Train ID', 'trainID', 1), ('Seat Number', 'seatNum', 1), ("Stations (splited by single '|')" ,'stations', 1),
+                      ("Prices (splited by single '|')", 'prices', 1), ('Start Time', 'startTime', 1), ("Travel Times (splited by single '|')", 'travelTimes', 1),
+                      ("Stopover Times (splited by single '|')", 'stopoverTimes', 1), ('Sale Date', 'saleDate', 1), ('Train Type', 'type', 1)]
+    release_train_form = [('Train ID', 'trainID', 1)]
+    query_train_form = [('Train ID', 'trainID', 1), ('Date', 'date', 1)]
+    delete_train_form = [('Train ID', 'trainID', 1)]
+    query_ticket_form = [('Time', 'time', 1), ('Start Station', 'start', 1), ('Destination Station', 'end', 1), ('Sort parameter', 'sort_param', 0)]
+    query_transfer_form = [('Time', 'time', 1), ('Start Station', 'start', 1), ('Destination Station', 'end', 1), ('Sort parameter', 'sort_param', 0)]
+    buy_ticket_form = [('Train ID', 'trainID', 1), ('Date', 'day', 1), ('From Station', 'ffrom', 1), ('To Station', 'to', 1),
+                       ('Number', 'number', 1), ('Queue? (0 or 1)', 'que', 1)]
+    refund_ticket_form = [('Order Number (From Last)', 'num', 1)]
+    login_form = [('Username', 'username', 1), ('Password', 'password', 1)]
+
+
+from uuid import uuid4
+
+class cookiePool:
+    def __init__(self):
+        self.pool = {}
+    def clean(self):
+        self.pool.clear()
+
+    def push(self, userName):
+        id = str(uuid4())
+        self.pool[id] = userName
+        return id
+    def erase(self, id):
+        self.pool.pop(id)
+
+    def check(self, id):
+        return id in self.pool
+    def query(self, id):
+        return self.pool[id]
+
 
