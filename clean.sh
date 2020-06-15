a694c876e2fb94e4c476ebc18aeb05d87302074f
Amagi_Yukisaki <xingzeyu@sjtu.edu.cn>
2020-06-15 22:28:20 +0800

    Added System Initialization Check

--- clean.sh
+++ clean.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-rm file_cur_users file_orders file_queue file_stations file_totalID file_trains_base file_trains_released file_users tmp
+rm file_cur_users file_orders file_queue file_stations file_totalID file_trains_base file_trains_released file_users tmp inited.txt
